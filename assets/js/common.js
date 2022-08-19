648738c46a7186d88751cb4144b24762db6b856f
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2022-08-19 17:03:02 +0100

    Defer and minify

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.secret").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.secret").toggleClass('open');
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
+$(document).ready((function(){$("a.abstract").click((function(){$(this).parent().parent().find(".abstract.secret").toggleClass("open")})),$("a.bibtex").click((function(){$(this).parent().parent().find(".bibtex.secret").toggleClass("open")})),$("a").removeClass("waves-effect waves-light")}));
